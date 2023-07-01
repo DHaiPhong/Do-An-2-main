0644aa0267e5a6efb69b2ea137e9235b9c0add00
Ngoctu1 <92847247+Ngoctu1@users.noreply.github.com>
2023-07-01 21:45:29 +0700

    sửa payment

--- resources/views/users/modun-user/payment.blade.php
+++ resources/views/users/modun-user/payment.blade.php
@@ -47,7 +47,7 @@
                             </div>
                             <div class="inputBox">
                                 <span>Số điện thoại :</span>
-                                <input name="phone" id="phone_number" type="text" value="{{ Auth::user()->phone }}" required placeholder="">
+                                <input name="phone" id="phone_number" type="text" value="" required placeholder="">
                             </div>
                             <div class="inputBox">
                             <span>Quận Huyện :</span>  
@@ -97,12 +97,22 @@
     <script>
   // Access the input element
   var input = document.getElementById('phone_number');
+input.addEventListener('input', function(){
+    if(this.value.includes("+")){
+        this.value = this.value.replace(/[^0-9+]/, '');
+        if(this.value.length>12) {
+            this.value = this.value.slice(0,12);
+        }
+    }else{
+        this.value = this.value.replace(/[^0-9]/, '');
+        if(this.value.length>10) {
+            this.value = this.value.slice(0,10);
+        }
+    }
+});
+  
 
-  // Listen for input into the phone number field
-  input.addEventListener('input', function() {
-    // Replace any non-digit characters with an empty string
-    input.value = input.value.replace(/\D/g, '');
-  });
+  
 </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" referrerpolicy="no-referrer"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js"></script>
